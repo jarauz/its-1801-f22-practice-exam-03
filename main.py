8db0ab89526d62e715ac654544a246d3ca9f4ce0
Julio Arauz <jarauz@alumni.pitt.edu>
2022-11-15 15:23:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import turtle
+
+casper = turtle.Turtle()
+listOfColors = [ "red", "green", "blue", "yellow"]
+
+for c in listOfColors:
+  print(c)
+
+for i in range(4):
+  print(i)
+
+# range(4) is internally this list [0,1,2,3]
